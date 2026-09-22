a31036f369f1dab195083471a978771dbfab0255
ARTERIO0 <artnedov@gmail.com>
2026-09-22 14:06:42 +0300

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,1 +1,25 @@
+self.addEventListener('push', function(event) {
+    if (event.data) {
+        const data = event.data.json();
+        const options = {
+            body: data.body,
+            icon: '/static/icon-192x192.png', // Необязательно, можно убрать
+            badge: '/static/badge-72x72.png', // Необязательно
+            vibrate: [100, 50, 100],
+            data: {
+                dateOfArrival: Date.now(),
+                primaryKey: '1'
+            }
+        };
+        event.waitUntil(
+            self.registration.showNotification(data.title, options)
+        );
+    }
+});
 
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('/')
+    );
+});
